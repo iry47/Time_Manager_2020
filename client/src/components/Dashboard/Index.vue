18a8d8967308cec1bab0dbc1f4e01d68908db014
geoffroy <geoffroy.huck@epitech.eu>
2020-11-04 00:59:53 +0100

    clean client

---
+++ client/src/components/Dashboard/Index.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-container fluid grid-list-md justify-center>
+    <v-layout class="mt-5" v-if="isUserLoggedIn && !admin" justify-center wrap>
+      <v-flex>
+      <grid-widgets/>
+    </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+/* GRIDSTER */
+import GridWidgets from "./Gridster/GridWidgets";
+
+/* SERVICE */
+import ServiceUser from "@/services/ApiAxios/Service/ServiceUser";
+import Service from "@/services/ApiAxios/Service/Service";
+
+export default {
+  data() {
+    return {
+      servicesUsers: null,
+      services: null
+    };
+  },
+  computed: {
+    ...mapState([
+      "isUserLoggedIn",
+      "user",
+      "servicesUser",
+      "servicesActive",
+      "admin",
+      "dark",
+      "grad"
+    ])
+  },
+  components: {
+    GridWidgets
+  },
+  props: {
+    main: {
+      type: Object,
+      require: true
+    }
+  },
+  methods: {
+  },
+  async mounted() {
+    try {
+      this.servicesUsers = (
+        await ServiceUser.getUserService(this.user.id)
+      ).data;
+      this.servicesUsers.forEach(data => {
+        if (data.ServiceId === 1) {
+          this.$store.dispatch("setServiceActiveOpenWeather", true);
+          this.$store.dispatch("setServiceUserTown", data.data);
+        } 
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+};
+</script>
+
+<style scoped></style>
---
+++ client/src/components/Dashboard/Widgets/WidgetsOpenWeather/WidgetMeteoDay.vue
@@ -0,0 +1,27 @@
+<template>
+  <widget v-if="isUserLoggedIn">
+    <div id="openweather-current"></div>
+  </widget>
+</template>
+
+<script>
+import OpenWeatherService from "@/services/OpenWeather/OpenWeatherService";
+import { mapState } from "vuex";
+
+export default {
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapState(["isUserLoggedIn", "user", "servicesUser", "route"])
+  },
+  methods: {
+  },
+  mounted() {
+    generateCurrentWeather(this.servicesUser.townOpenWeather)
+  },
+  watch: {}
+};
+</script>
+
+<style scoped></style>
